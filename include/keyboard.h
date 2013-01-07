b6666376479962692ee74b9f78def55ff6c37fb4
loveywm <490054348@qq.com>
2013-01-07 13:54:32 +0800

    Revert "Add all code of e90"
    This reverts commit 11dc61f539a1faf0545b3bc228fd5ea23086bf98.

--- include/keyboard.h
+++
@@ -1,7 +0,0 @@
-#ifndef __KEYBOARD_H__
-#define __KEYBOARD_H__
-
-#define IOCTL_KEYBOARD_PAUSE	1
-#define IOCTL_KEYBOARD_RESUME	2
-
-#endif /*__KEYBOARD_H__*/
--- source/md5_hash.h
+++
@@ -1,6 +0,0 @@
-#ifndef __MD5_HASH__
-#define __MD5_HASH__
-
-unsigned int sb_hash(const uchar *p, int n);
-
-#endif /*__MD5_HASH__*/
